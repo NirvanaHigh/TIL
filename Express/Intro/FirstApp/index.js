27436c97c66fffd6381e0d47f75c357ad1ba398f
NirvanaHigh <nirvanah557@gmail.com>
2022-06-02 18:39:25 +0900

    Express Day 1

---
+++ Express/Intro/FirstApp/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+
+app.use((req, res) => {
+    console.log("WE GOT A NEW REQUEST!")
+    res.send('<h1>This is my webpage!</h1>')
+})
+
+app.listen(8080, () => {
+    console.log("LISTENING ON PORT 8080!")
+})
