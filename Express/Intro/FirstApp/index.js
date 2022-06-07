e4daa14fb2bc6161f6b2324c6280d0c0f742c539
NirvanaHigh <nirvanah557@gmail.com>
2022-06-07 18:23:28 +0900

    Express Day 2

--- Express/Intro/FirstApp/index.js
+++ Express/Intro/FirstApp/index.js
@@ -1,9 +1,35 @@
 const express = require("express")
 const app = express()
 
-app.use((req, res) => {
-    console.log("WE GOT A NEW REQUEST!")
-    res.send('<h1>This is my webpage!</h1>')
+// app.use((req, res) => {
+//     console.log("WE GOT A NEW REQUEST!")
+//     res.send('<h1>This is my webpage!</h1>')
+// })
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the home page!')
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`)
+})
+
+app.post('/cats', (req, res) => {
+    res.send('POST REQUEST TO /cats!!!! THIS IS DIFFERENT THAN A GET REQUEST!')
+})
+
+
+app.get('/cats', (req, res) => {
+    res.send('MEOW!!')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF!')
+})
+
+app.get('*', (req, res) => {
+    res.send(`I don't know that path!`)
 })
 
 app.listen(8080, () => {
