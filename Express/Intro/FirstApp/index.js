a049ef84522572004d72abecbba1421757672b4d
NirvanaHigh <nirvanah557@gmail.com>
2022-06-08 18:52:08 +0900

    Express Day 3

--- Express/Intro/FirstApp/index.js
+++ Express/Intro/FirstApp/index.js
@@ -7,13 +7,19 @@ const app = express()
 // })
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the home page!')
+    res.send('Welcome to the home page!!!')
 })
 
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
     res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`)
 })
+
+app.get('/r/:subreddit/:postId', (req, res) => {
+    const { subreddit, postId } = req.params;
+    res.send(`<h1>Viewing Post ID: ${postId} on the ${subreddit} subreddit</h1>`)
+})
+
 
 app.post('/cats', (req, res) => {
     res.send('POST REQUEST TO /cats!!!! THIS IS DIFFERENT THAN A GET REQUEST!')
@@ -27,6 +33,16 @@ app.get('/cats', (req, res) => {
 app.get('/dogs', (req, res) => {
     res.send('WOOF!')
 })
+
+app.get('/search', (req, res) => {
+    const { q } = req.query;
+    if (!q) {
+        res.send('NOTHING FOUND IF NOTHING SEARCHED!')
+    } else {
+        res.send(`<h1>Search results for: ${q}</h1>`)
+    }
+})
+
 
 app.get('*', (req, res) => {
     res.send(`I don't know that path!`)
