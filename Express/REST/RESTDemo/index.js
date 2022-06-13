72852cc78821287c5ce9bd60529d0c31cbc48de1
NirvanaHigh <nirvanah557@gmail.com>
2022-06-13 18:29:48 +0900

    Express Day 7

--- Express/REST/RESTDemo/index.js
+++ Express/REST/RESTDemo/index.js
@@ -1,43 +1,52 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 
-
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 const comments = [
-    {
-        username: 'Todd',
-        comment: 'lol that is so funny!'
-    },
-    {
-        username: 'Skyler',
-        comment: 'I like to go birdwatching with my dog'
-    },
-    {
-        username: 'Sk8erBoi',
-        comment: 'Plz delete your account, Todd'
-    },
-    {
-        username: 'onlysayswoof',
-        comment: 'woof woof woof'
-    }
-]
+  {
+    id: 1,
+    username: "Todd",
+    comment: "lol that is so funny!",
+  },
+  {
+    id: 2,
+    username: "Skyler",
+    comment: "I like to go birdwatching with my dog",
+  },
+  {
+    id: 3,
+    username: "Sk8erBoi",
+    comment: "Plz delete your account, Todd",
+  },
+  { id: 4, username: "onlysayswoof", comment: "woof woof woof" },
+];
 
-app.get('/comments', (req, res) => {
-    res.render('comments/index', { comments });
-})
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { comments });
+});
 
-app.get('/tacos', (req, res) => {
-    res.send("GET /tacos response")
-})
-app.post('/tacos', (req, res) => {
-    const { meat, qty } = req.body
-    res.send(`OK, here are your ${qty} ${meat} tacos`)
-})
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new");
+});
+
+app.post("/comments", (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment });
+  res.redirect("/comments");
+});
+
+app.get("/tacos", (req, res) => {
+  res.send("GET /tacos response");
+});
+app.post("/tacos", (req, res) => {
+  const { meat, qty } = req.body;
+  res.send(`OK, here are your ${qty} ${meat} tacos`);
+});
 app.listen(3000, () => {
-    console.log("On Port 3000!")
-})
+  console.log("On Port 3000!");
+});
