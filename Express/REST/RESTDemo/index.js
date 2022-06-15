e29cecd834bfc7bb7e267437a28304e7f91c12bd
NirvanaHigh <nirvanah557@gmail.com>
2022-06-15 18:45:47 +0900

    Express Day 8

--- Express/REST/RESTDemo/index.js
+++ Express/REST/RESTDemo/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const { v4: uuid } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -9,21 +10,25 @@ app.set("view engine", "ejs");
 
 const comments = [
   {
-    id: 1,
+    id: uuid(),
     username: "Todd",
     comment: "lol that is so funny!",
   },
   {
-    id: 2,
+    id: uuid(),
     username: "Skyler",
     comment: "I like to go birdwatching with my dog",
   },
   {
-    id: 3,
+    id: uuid(),
     username: "Sk8erBoi",
     comment: "Plz delete your account, Todd",
   },
-  { id: 4, username: "onlysayswoof", comment: "woof woof woof" },
+  {
+    id: uuid(),
+    username: "onlysayswoof",
+    comment: "woof woof woof",
+  },
 ];
 
 app.get("/comments", (req, res) => {
@@ -36,8 +41,14 @@ app.get("/comments/new", (req, res) => {
 
 app.post("/comments", (req, res) => {
   const { username, comment } = req.body;
-  comments.push({ username, comment });
+  comments.push({ username, comment, id: uuid() });
   res.redirect("/comments");
+});
+
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/show", { comment });
 });
 
 app.get("/tacos", (req, res) => {
